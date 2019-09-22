6ab5c18f7357e0374cdfd1c124cdf5ccf4db73de
0henri0 <zxtinkerxz1@gmail.com>
2019-09-22 10:41:32 +0700

    connect to mlab

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-  db: process.env.MONGODB_URL || 'mongodb://localhost:27017/myblog'
+  db: process.env.MONGODB_URL || 'mongodb+srv://thainv1612:thaicutevodoi@cluster0-xdzer.mongodb.net/myblog?retryWrites=true&w=majority'
 }
