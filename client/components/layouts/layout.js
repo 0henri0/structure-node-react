4fc13379ca3aa727467fd9adf3f1b9b396aabf61
thai-dep-trai <zxtinkerxz1@gmail.com>
2019-10-01 09:41:25 +0700

    clean code

--- client/components/layouts/layout.js
+++ client/components/layouts/layout.js
@@ -3,7 +3,7 @@ import Header from './header';
 import Footer from './footer';
 import { Fragment } from 'react';
 import PropTypes from 'prop-types';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../static/app';
 
 const Layout = ({ children, title = 'This is the default title' }) => (
