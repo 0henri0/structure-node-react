dda9a415096b86d8b21313a6e689412f870502fb
trantrongbinh <tranbinhbak@gmail.com>
2019-10-09 17:37:30 +0700

    View web base

---
+++ client/components/Web/Home/BlogLeftSidebar.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+import '../../../styles/blog_left_sidebar.less';
+
+const BlogRightSidebar = () => {
+  return (
+    <div className="blog_left_sidebar">
+      <article className="blog_wrap">
+        <div className="blog_img">
+          <img className="img-fluid" src="/static/img/home-blog/blog-1.jpg" alt="" />
+        </div>
+        <div className="blog_text">
+          <div className="blog_text_inner">
+            <div className="cat">
+              <a className="cat_btn" href="#">Gadgets</a>
+              <a href="#"><Icon type="calendar" /> March 14, 2018</a>
+            </div>
+            <a href="#">
+              <h4>Nest Protect: 2nd Gen Smoke + CO Alarm</h4>
+            </a>
+            <p>
+              Vào lúc 15h30 ngày 8/10, tên cướp đội mũ bảo hiểm, đeo khẩu trang đi xe máy Yamaha Exciter xông vào tiệm vàng Lương Oanh ở phường Mạo Khê, thị xã Đông Triều, Quảng Ninh. Một lúc sau, tên này xách balo chạy ra cửa nhưng làm rơi nhiều cọc tiền. Hai phụ nữ trong tiệm vàng hô hoán, chạy ra giằng được chiếc túi.
+            </p>
+            <a className="blog_btn" href="#">Read More</a>
+          </div>
+        </div>
+      </article>
+      <div className="row">
+        <div className="col-md-6">
+          <article className="blog_wrap small">
+            <div className="blog_img">
+              <img className="img-fluid" src="/static/img/home-blog/blog-small-1.jpg" alt="" />
+            </div>
+            <div className="blog_text">
+              <div className="blog_text_inner">
+                <div className="cat">
+                  <a className="cat_btn" href="#">Gadgets</a>
+                  <a href="#"><Icon type="calendar" /> March 14, 2018</a>
+                </div>
+                <a href="single-blog.html">
+                  <h4>Nest Protect 2nd Gen Smoke CO Alarm</h4>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua.</p>
+                <a className="blog_btn" href="#">Read More</a>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div className="col-md-6">
+          <article className="blog_wrap small">
+            <div className="blog_img">
+              <img className="img-fluid" src="/static/img/home-blog/blog-small-2.jpg" alt="" />
+            </div>
+            <div className="blog_text">
+              <div className="blog_text_inner">
+                <div className="cat">
+                  <a className="cat_btn" href="#">Gadgets</a>
+                  <a href="#"><Icon type="calendar" /> March 14, 2018</a>
+                </div>
+                <a href="single-blog.html">
+                  <h4>Nest Protect 2nd Gen Smoke CO Alarm</h4>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua.</p>
+                <a className="blog_btn" href="#">Read More</a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogRightSidebar;
---
+++ client/components/Web/Layouts/Footer.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+import '../../../styles/footer.less';
+
+export default function Footer() {
+  return (
+    <footer id="footer" className="dark">
+      <div className="footer-wrap">
+        <Row>
+          <Col md={6} sm={24} xs={24}>
+            <div className="footer-center">
+              <h2>BKFA Blog</h2>
+              <div>
+                <a target="_blank " href="https://github.com/ant-design/ant-design">
+                  GitHub
+                </a>
+              </div>
+              <div>
+                <a href="http://pro.ant.design">Facebook</a>
+              </div>
+              <div>
+                <a href="http://mobile.ant.design">Profile</a>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} sm={24} xs={24}>
+            <div className="footer-center">
+              <h2>Blog bạn bè</h2>
+              <div>
+                <a href="http://scaffold.ant.design">ABC Blog</a>
+                <span> -  Chia sẻ về công nghệ </span>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="http://ux.ant.design">vlxxx</a>
+                <span> - Chia sẻ video </span>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} sm={24} xs={24}>
+            <div className="footer-center">
+              <h2>Help</h2>
+              <div>
+                <a href="/changelog">
+                  Change Log
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/wiki/FAQ">
+                  GitHub
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design">
+                  Chat Room (English)
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} sm={24} xs={24}>
+            <div className="footer-center">
+              <h2>
+                <img className="title-icon" src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg" alt="" />
+                More About Us
+              </h2>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://antv.alipay.com/">Mục đích</a>
+                <span> - Chia sẻ, chia sẻ và chia sẻ ...</span>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://eggjs.org/">Slogan</a>
+                <span> - Thất bại không phải là ngõ cụt!</span>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <Row className="bottom-bar">
+        <Col md={4} sm={24} />
+        <Col md={20} sm={24}>
+          <span style={{ marginRight: 12 }}>Personal Blog</span>
+          <span style={{ marginRight: 12 }}>Copyright © BKFA</span>
+        </Col>
+      </Row>
+    </footer>
+  );
+}
